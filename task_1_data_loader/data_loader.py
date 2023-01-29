a60200d01e1e246a49257864f99fb877ade3e447
Asim Aryal <asim.aryal99@gmail.com>
2023-01-29 19:48:45 +0000

    Task 1 working

--- task_1_data_loader/data_loader.py
+++ task_1_data_loader/data_loader.py
@@ -14,20 +14,23 @@ class CustomDataLoader(Dataset):
         root_dir: Path = Path("C:/Users/asima/PycharmProjects/AAGI_Prac/data_terrain/"),
     ):
         self.classes = ["desert", "green", "cloudy", "water"]
-        self.list_of_filenames = []
-
         self.size = size
         self.root_dir = root_dir
         self.folder_sizes = self.check_and_get_folder_sizes(size, self.classes)
+        self.filenames = np.transpose(
+            pd.DataFrame.from_dict(
+                dict(zip(self.classes, self.get_list_of_filenames(size))),
+                orient="index",
+            )
+        )
+
+    def get_list_of_filenames(self, size: int):
+        list_of_filenames = []
         for folder in self.classes:
             size_of_folder = self.folder_sizes[folder]
             size_to_get = size_of_folder if size_of_folder < size else size
-            self.list_of_filenames.append(self.get_all_filenames(folder, size_to_get))
-        self.filenames = np.transpose(
-            pd.DataFrame.from_dict(
-                dict(zip(self.classes, self.list_of_filenames)), orient="index"
-            )
-        )
+            list_of_filenames.append(self.get_all_filenames(folder, size_to_get))
+        return list_of_filenames
 
     def get_all_filenames(self, folder_name: str, number_of_files: int) -> list[str]:
         return random.sample(self.get_filenames(folder_name), number_of_files)
@@ -45,8 +48,10 @@ class CustomDataLoader(Dataset):
         return dict(zip(folders, sizes))
 
     @staticmethod
-    def export_to_csv(filenames_df: pd.DataFrame):
-        filenames_df.to_csv("filenames.csv", sep=",")
+    def export_to_csv(filenames_df: pd.DataFrame, save_name: str) -> None:
+        save_name = save_name+".csv" if ".csv" not in save_name else save_name
+        filenames_df.to_csv(save_name, sep=",")
+        print(f"{save_name} Saved.")
 
     def __len__(self):
         return len(self.filenames)
@@ -57,9 +62,7 @@ class CustomDataLoader(Dataset):
     def __iter__(self):
         return self
 
-    def __next__(
-        self,
-    ):
+    def __next__(self):
         return CustomDataLoader(self.size)
 
 
--- task_1_data_loader/main.py
+++ task_1_data_loader/main.py
@@ -6,9 +6,15 @@ if __name__ == "__main__":
     batch_size = 32
     dataset = CustomDataLoader(100)
     print(f"Size of dataset: {dataset.__len__()}")
-    train_set, test_set = torch.utils.data.random_split(dataset, [80, 20])
+    split_ratio = 0.8
+    train_size = int(split_ratio * dataset.__len__())
+    test_size = len(dataset) - train_size
+    train_set, test_set = torch.utils.data.random_split(
+        dataset, [train_size, test_size]
+    )
     train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
     print(f"images per batch: {train_loader.__len__()}")
     test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
     new_set = next(CustomDataLoader(1000))
     print("Size of new set: ", new_set.__len__())
+    dataset.export_to_csv(dataset.filenames, "filenames")
